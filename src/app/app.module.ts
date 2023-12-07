ca4018a7b35cd64d55cc8e605dbf75f7eb1bf5eb
ArlintonAr <arlintonarturodelgadoflores@gmail.com>
2023-12-07 08:39:43 -0500

    Subiendo proyecto creado en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
+
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
+    HttpClientModule,
+
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    GifsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
